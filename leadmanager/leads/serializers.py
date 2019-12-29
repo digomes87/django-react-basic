b147a30053e781d4ce201540ad5aa40e08cc1696
Diego Go <diego.gomes87@gmail.com>
2019-12-29 12:12:54 -0300

    many error

--- leadmanager/leads/serializers.py
+++ leadmanager/leads/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
-from leads.models import Lead
+from leads.models import Lead 
 
-# Lead serializer
+# Lead Serializer
 class LeadSerializer(serializers.ModelSerializer):
   class Meta:
-    model = Lead
+    model = Lead 
     fields = '__all__'
